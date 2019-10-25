2210e224ea7682c2ca19809985b0e11fd85686c9
Nikita Govorov <nikita.govorov@gmail.com>
2019-10-25 12:38:11 +1000

    Initial commit.

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,35 @@
+import { By } from '@angular/platform-browser';
+import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+
+  let fixture;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        ReactiveFormsModule
+      ],
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+  }));
+
+  it('correct dateStart should be valid', () => {
+    const inputDateStart = fixture.debugElement.query(By.css('[formControlName=dateStart]')).nativeElement;
+    inputDateStart.value = 'valid';
+    inputDateStart.dispatchEvent(new Event('input'));
+    inputDateStart.dispatchEvent(new Event('change')); // redundant event, can be removed
+    fixture.detectChanges();
+    expect(inputDateStart.classList.contains('ng-invalid')).toBeFalsy('dateStart should be valid');
+  });
+
+});
---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  form = new FormGroup({ dateStart: new FormControl('boom', Validators.minLength(5)) });
+}
